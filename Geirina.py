3863688b88214ca9ccf3872e3f3142765303f86f
Jiajun <jiajunduan.ee@gmail.com>
2020-05-14 08:35:27 -0700

    GEIRINA Baseline updated

--- Geirina.py
+++ Geirina.py
@@ -77,7 +77,7 @@ class Geirina(DoNothingAgent):
                  verbose=False,
                  data_dir=".",
                  model_name="geirina",
-                 restore=True
+                 restore=False
                  ):
         DoNothingAgent.__init__(self, action_space)
         self.do_nothing = self.action_space()
@@ -167,6 +167,9 @@ class Geirina(DoNothingAgent):
         if name == 'l2rpn_case14_sandbox':
           self.line_map = [0,1,2,3,4,5,6,15,16,17,9,8,7,18,19,11,10,12,13,14]
           load_map = [0,1,2,3,4,5,6,7,8,9,10]
+        elif name == "l2rpn_2019":
+          self.line_map = [i for i in range(14)]
+          load_map = [0,1,2,3,4,5,6,7,8,9,10]          
         else:
           self.line_map = [0,1,7,8,9,10,11,15,16,17,14,13,12,18,19,3,2,4,5,6]
           load_map = [0,1,3,4,5,6,7,8,9,10,2]
--- train.py
+++ train.py
@@ -64,10 +64,18 @@ if __name__ == "__main__":
     from lightsim2grid.LightSimBackend import LightSimBackend
     backend = LightSimBackend()
 
+    TEST_DEV_ENVS = ["l2rpn_case14_sandbox", "rte_case14_realistic", "rte_case14_redisp", 
+    "rte_case14_test", "case14_realistic", "case14_redisp","case14_test", "l2rpn_2019"] 
+
+
     env = make("l2rpn_case14_sandbox"
                # gamerules_class=AlwaysLegal,
                #backend=backend
                )
+    
+    if env.name not in TEST_DEV_ENVS:
+        raise RuntimeError("This baseline only works for: {} ".format(TEST_DEV_ENVS)) 
+
 
     train(env,
           name="Geirina",
